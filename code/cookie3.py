1c042a6a43773bc344c3c5b0d4bacf78354280ef
phiaseitz <sophia.seitz@students.olin.edu>
2014-09-16 11:40:37 -0400

    Taking 11 chocolate cookies in cookie3.py

--- code/cookie3.py
+++ code/cookie3.py
@@ -30,15 +30,21 @@ def main():
     bowl2 = thinkbayes2.Hist(dict(vanilla=20, chocolate=20))
     pmf = Cookie([bowl1, bowl2])
 
-    print('After 1 vanilla')
-    pmf.Update('vanilla')
-    for hypo, prob in pmf.Items():
-        print(hypo, prob)
+    # print('After 1 vanilla')
+    # pmf.Update('vanilla')
+    # for hypo, prob in pmf.Items():
+    #     print(hypo, prob)
 
-    print('\nAfter 1 vanilla, 1 chocolate')
-    pmf.Update('chocolate')
-    for hypo, prob in pmf.Items():
-        print(hypo, prob)
+    # print('\nAfter 1 vanilla, 1 chocolate')
+    # pmf.Update('chocolate')
+    # for hypo, prob in pmf.Items():
+    #     print(hypo, prob)
+
+    for numberCookie in range(1,12):
+        print('\nAfter %d chocolate' % numberCookie)
+        pmf.Update('chocolate')
+        for hypo, prob in pmf.Items():
+            print(hypo, prob)
 
 
 if __name__ == '__main__':
